d792c43c625e1bab1010a4ee15fba47b5a66d17e
jsturgeo <juliesturgeon@gmail.com>
2016-11-09 09:49:44 -0800

    added code from slides, checked that it runs

--- train_model.m
+++ train_model.m
@@ -2,14 +2,42 @@
 % author: your name
 % date: date for release this code
 
-clear; clc;
+%clear; clc;
 
 %% Section 1: preparation before training
 
 % section 1.1 read file 'train.txt', load data and vocabulary by using function read_data()
 
 [data, wordMap] = read_data;
+
+
+% Create initial word embedding
+d = 5;
 numWords = length(wordMap);
+% random sample from normal distribution 
+% with mean=0, variance=0.1
+T = normrnd(0, 0.1, [numWords, d]);
+
+% Initialize filters
+filterSizes = [2, 3, 4];
+numFilterSizes = length(filterSizes);
+numFilters = 2;
+
+convW = cell(length(filterSizes), 1);
+convB = cell(length(filterSizes), 1);
+
+for i = 1:length(filterSizes)
+    filterSize = filterSizes(i);
+    % initialize W with: FW x FH x FC x K
+    convW{i} = normrnd(0, 0.1, [filterSize, d, 1, numFilters]);
+    convB{i} = zeros(numFilters, 1);
+end
+
+% Initialize output layer
+totalFilters = length(filterSizes) * numFilters;
+numClasses = 2;
+outW = normrnd(0, 0.1, [totalFilters, numClasses]);
+outB = zeros(numClasses, 1);
 
 
 %% Section 2: training
@@ -19,25 +47,46 @@ numWords = length(wordMap);
 %       vl_nnconv(), vl_nnpool(), vl_nnrelu(), vl_nnconcat(), and vl_nnloss()
 
 for ind=1:length(data)
-    [i, sentence, label] = data{ind,:};
-    
-    %% Encode a one-hot vector for
+    [i, sentence, label] = data{ind,:}
     sentenceLength = length(sentence);
     sentenceWordInds = zeros(sentenceLength, 1);
-    %Get index of each word in the sentence
+    % get index for each word in the sentence
     for w=1:sentenceLength
         sentenceWordInds(w) = wordMap(strjoin(sentence(w)));
     end
-    % Build one-hot matrix where each row is a one-hot vector for each word
-    ohVec = sparse(1:sentenceLength, sentenceWordInds, ones(sentenceLength, 1), sentenceLength, numWords);
+    X = T(sentenceWordInds, :);
+    
+    poolRes = cell(1, numFilterSizes);
+    cache = cell(2, numFilterSizes);
+    
+    for j=1:numFilterSizes
+        % convolutional operation
+        if filterSizes(j) > sentenceLength
+            cache{2, j} = 0;
+            cache{1, j} = 0;
+            poolRes{j} = 0; 
+           continue 
+        end
+        conv = vl_nnconv(X, convW{j}, convB{j});
+        
+        % apply activation function: relu
+        relu = vl_nnrelu(conv);
+        
+        % 1-max pooling operation
+        convSize = size(conv);
+        pool = vl_nnpool(relu, [convSize(1), 1]);
+        
+        % important: keep these values for back-prop
+        cache{2, j} = relu;
+        cache{1, j} = conv;
+        poolRes{j} = pool;  
+    end
+    
+
     
     %% for each example in train.txt do
     %% section 2.1 forward propagation and compute the loss
-    % Create a bank of 2 <numWords> x 3 filters (to start with)
-    w = randn(numWords,2,1,2,'single');
-    
-    % Apply the convolution operator
-    y = vl_nnconv(ohVec, w, []) ; %bias terms?
+
 
     %% section 2.2 backward propagation and compute the derivatives
     % TODO: your code
@@ -45,3 +94,4 @@ for ind=1:length(data)
     %% section 2.3 update the parameters
     % TODO: your code
 end %end for
+    
